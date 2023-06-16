2d3ad7fefd129960b19f80686dfc2dbddc466d87
Mqmba <nguyenhoanghuy9a5@gmail.com>
2023-06-16 11:43:31 +0700

    lam lai layout

---
+++ EventManagement/src/java/controller/config.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author mamba
+ */
+public class config {
+    public static final String LAYOUT = "/WEB-INF/views/layouts/main.jsp";
+}
